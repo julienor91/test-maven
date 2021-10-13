5ec2089864fe0ef59997cf6f95be7f94a2b21cb6
julienor91 <julienor91@hotmail.fr>
2021-10-13 16:24:14 +0200

    Branche de test <profil> du fichier pom.xml

--- src/main/java/org/exemple/demo/App.java
+++ src/main/java/org/exemple/demo/App.java
@@ -1,13 +1,29 @@
 package org.exemple.demo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class App {
+    
+	public static void main( String[] args ) throws IOException {
+        
+		System.out.println( "Hello World!" );
+		
+		Properties vProp = new Properties();
+		InputStream vInputStream = null;
+		
+		try {
+			vInputStream = App.class.getResourceAsStream("/info.properties");
+			vProp.load(vInputStream);
+		} finally {
+			if (vInputStream != null) {
+				vInputStream.close();
+			}
+		}
+		
+		System.out.println("Application version : " + vProp.getProperty("org.exemple.demo.version", "?"));
+		
     }
+	
 }
